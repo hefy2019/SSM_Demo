f26315a228430f3fca4bcf6e7f0fa389eea2765d
hefy <282971962@qq.com>
2019-07-10 23:21:40 +0800

    拦截器  使用afterCompletion拦截日志文件

--- springmvc-interceptor/src/interceptor/TestInterceptor.java
+++ springmvc-interceptor/src/interceptor/TestInterceptor.java
@@ -5,7 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ * 拦截器 
+ * @author hfy
+ * 2019年7月10日
+ */
 public class TestInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -19,7 +23,7 @@ public class TestInterceptor implements HandlerInterceptor {
 			ModelAndView modelAndView) throws Exception {
 		System.out.println("postHandel 方法在控制器的处理请求方法调用之后，解析视图之前执行");
 	}
-
+	//用次方法拦截日志文件 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
